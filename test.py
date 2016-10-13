6d9e8170bd0f5167931562c54d9ea315b0ecb4fc
Jessica Morris <jess.d.morris@hotmail.com>
2016-10-13 11:23:27 -0400

    test.py - Works properly! Does math!
    Proves my formula for Q6 is correct now.

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python
 import itertools
+from math import factorial
 
 error_char = 'X'
 good_char = 'o'
 
+def ncr(n,k):
+    numerator=factorial(n)
+    denominator=(factorial(k)*factorial(n-k))
+    answer=numerator//denominator
+    return answer
+
 def print2D(string, n, k):
 	for i in range(0, n*k, k):
-		print string[i:i+k]
+		print(string[i:i+k])
 
 def calcSum(n,k):
 	total = 0
 	
-	n_vals = range(n-(n-2)-1, n-0)
-	k_vals = range(k-(k-2)-1, k-0)
+	n_vals = range(n-(n-2)-1, n)
+	k_vals = range(k-(k-2)-1, k)
 	
 	for n_val in n_vals:
 		for k_val in k_vals:
@@ -28,7 +35,7 @@ def computeErrorProb(n,k):
 	permutations = set(itertools.permutations(error_char*4 + (n*k-4)*good_char))
 	total_permutations = len(permutations)
 	
-	print "--- Undetectable errors ---"
+	print("--- Undetectable errors ---")
 	for perm in permutations:
 		error_indices = list([])
 		
@@ -38,16 +45,18 @@ def computeErrorProb(n,k):
 				
 		# Determine if these errors are undetectable - vertices in a rectangle
 		# 	fix tomorrow
-		# Checks - 0 and 1 in same row, 0 and 2 in same col, 1 and 3 in same col, 
-		if error_indices[0]/n == error_indices[1]/n and error_indices[0]%n == error_indices[2]%n and error_indices[1]%n == error_indices[3]%n and error_indices[2]/n == error_indices[3]/n:
+		# Checks - 0 and 1 in same row, 0 and 2 in same col, 1 and 3 in same col, 2 and 3 in the same row
+		if (error_indices[0]//k == error_indices[1]//k) and (error_indices[0]%k == error_indices[2]%k) and (error_indices[1]%k == error_indices[3]%k) and (error_indices[2]//k == error_indices[3]//k):
 			total_undec += 1
 			print2D(perm,n,k)
-			print ''
+			print('')
 									
-	print "Total unique permutations: " + str(total_permutations)
-	print "Total undetectable errors: " + str(total_undec)
-	print "Sum(k-p-1)(n-q-1): " + str(calcSum(n,k))
-	print "nk(nk-1)(nk-2)(nk-3) = " + str(n*k*(n*k - 1)*(n*k - 2)*(n*k - 3))
+	print("PROBABILITY OF UNDETECTION: " + str(total_undec/total_permutations))
+	print("Total unique permutations: " + str(total_permutations))
+	print("Total undetectable errors: " + str(total_undec))
+	print("Sum(k-p-1)(n-q-1): " + str(calcSum(n,k)))
+	print("nk nCr 4: " + str(ncr(n*k, 4)))
+	print("nk(nk-1)(nk-2)(nk-3) = " + str(n*k*(n*k - 1)*(n*k - 2)*(n*k - 3)))
 
 if __name__ == "__main__":
 	computeErrorProb(3,4)
